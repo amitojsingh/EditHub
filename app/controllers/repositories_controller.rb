4b4d3eefd90b81aa13837e7f67fbad213b66173d
Amitoj Singh <amitojsingh95@gmail.com>
2017-02-20 16:08:04 +0530

    cured routes

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -9,31 +9,39 @@
     def create
       @repository=Repository.create(repository_params)
 
-      if @repository.save
-        puts "successfully saved"
-      Zip::File.open("#{@repository.upload.path}") do |file|
+        if @repository.save
+          puts "successfully saved"
+          Zip::File.open("#{@repository.upload.path}") do |file|
 
-          file.each do |entry|
-            entry_path=Rails.root.join("public/system/repositories/uploads/extract", entry.name)
-            puts "#{entry_path}"
+              file.each do |entry|
+                entry_path=Rails.root.join("public/system/repositories/uploads/extract", entry.name)
                         FileUtils.mkdir_p(File.dirname(entry_path))
                         entry.extract(entry_path) unless File.exist?(entry_path)
-          end
-          #  puts "Extracting #{entry.name}"
+                      end
+                    end
+            #  puts "Extracting #{entry.name}"
             #if entry.directory?
             #  puts "#{entry.name}is folder"
             #elsif entry.symlink?
             #  puts "#{entry.name} is symlink"
             #elsif entry.file?
             #  puts "#{entry.name} is file
-          end
+            flash[:success]="Successfully created"
+          redirect_to repositories_path
+        else
+          flash[:danger]="Unable to extract"
+          redirect_to repositories_new_path
         end
-      end
+    end
       def show
+        @repository=Repository.find(params[:id])
+        path=Rails.root.join("public/system/repositories/uploads/extract" , @repository.upload_file_name)
+        Dir.chdir("public/system/repositories/uploads/extract/#{@repository.upload_file_name}")
+        puts" #{Dir.pwd}"
       end
 
     private
     def repository_params
-    params.permit(:upload)
+    params.permit(:id,:upload)
   end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resource 'repositories'
-  get 'repositories/new'
-  get 'repositories/index'
-  root "repositories#new"
+ get 'repositories/new'
+ get 'repositories/:id', to: "repositories#show", as: "repository"
+ get "repositories", to: "repositories#index", as: "repositories"
+ post "repositories", to: "repositories#create"
+ root "repositories#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
