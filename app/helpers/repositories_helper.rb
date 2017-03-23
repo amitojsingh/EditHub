91288f46a7f93706390d51c1f112a3365b0cbdba
Amitoj Singh <amitojsingh95@gmail.com>
2017-03-23 15:54:43 +0530

    listing directories completed

--- app/helpers/repositories_helper.rb
+++ app/helpers/repositories_helper.rb
@@ -1,95 +1,124 @@
 module RepositoriesHelper
 
 def traversal
-  @root={}
-  @array=[]
-  @folder ={}
-  @combine= []
-  @newarr= []
-  @file.each do |f|
-    if f.include?('/')
-      @combine<<f
-      elsif(File.file?(f))
-        @array<<f
+dirMap={}
+partsOfPath=[]
+dirTree={}
+parents=[]
+tree=[]
+newtree=[]
+@finalTree={}
+@html=''
+@file.each do |path|
+  partsOfPath=rasta(path)
+  tree<<path.split('/').first
+  puts "files or directories inside #{path}"
+  (0...partsOfPath.length).each do |i|
+    fileDir=partsOfPath[i];
+    puts "#{fileDir}"
+    if (!dirMap[fileDir])
+      dirMap[fileDir]=[partsOfPath[i+1]]
+    else
+      if (!dirMap[fileDir].include?(partsOfPath[i+1]))
+      dirMap[fileDir].push(partsOfPath[i+1])
     end
   end
-  while @combine.any? do
-    puts"combine#{@combine}"
-    text=@combine.first.split('/')
-    @combine=get_array(@combine,text)
-    puts "newcombine= #{@combine}"
-  end
-  @array<<@folder
 end
+end
+puts "#{dirMap}"
 
-def get_array(combine,text)
-  newarray=[]
-  subdir=[]
-  combine.each do |c|
-     if c.count('/')>1
-         subdir<<c
-         combine-=[c]
-    elsif c.include?(text.first)
-      @store=text.first
-      subfile=c.split('/')
-      newarray<<subfile.second
-      combine-=[c]
-      puts "newarray = #{newarray}"
+dirMap.each do |key,value|
+  if(!(dirMap[key].length===1 && dirMap[key][0]=== nil))
+    parents<<key
+  end
+end
+puts "parents are #{parents}"
+
+parents.each do |key|
+  dirMapValue=dirMap[key]
+  dirMap.each do |jkey,jvalue|
+    isKeyInValue=dirMap[jkey].include?(key)
+    if (isKeyInValue)
+      indexOfKeyInValue=dirMap[jkey].index("#{key}")
+      puts "#{jkey} contains #{key} at index #{indexOfKeyInValue}"
+      myobj={}
+      myobj[key]=dirMap[key]
+      puts "object to add at index #{indexOfKeyInValue} is #{myobj}"
+      dirMap[jkey][indexOfKeyInValue]={}
+      dirMap[jkey][indexOfKeyInValue][key]=dirMap[key]
+      dirTree[jkey]=dirMap[jkey]
+
+    else
+      dirTree[jkey]=dirMap[jkey]
     end
   end
-     child=loopdir(subdir)
-     puts "child = #{child}"
-     if child==@store
-       newarray<<@shash
-     end
-     @folder[@store]=newarray
-  return combine
 end
+#puts "#{dirTree}"
 
-def loopdir(subdir)
-  while subdir.any? do
-    puts "subdir #{subdir}"
-    parent=subdir.first.split('/')
-    subdir=get_subdir(subdir,parent.second)
-    puts "new subdir #{subdir}"
-    return parent.first
+newtree<<tree.uniq
+newtree.each do |element|
+  element.each do |e|
+    @finalTree[e]=dirTree[e]
   end
 end
-
-def get_subdir(subdir,singleparent)
-  newarray=[]
-  @shash={}
-  children=[]
-  subarray=[]
-  subdir.each do |s|
-    c=s.split('/')
-    c.shift
-    subarray<<s
-      puts "after splitting #{c}"
-      children<<c.first
-
-      subdir-=[s]
+puts "#{@finalTree}"
+@html<<"<ul>"
+if @finalTree.is_a?(Hash)
+  mainlist=callHash(@finalTree)
+end
+@html<<mainlist
+@html<<"</ul>"
+return @html
+end
+def rasta(path)
+  x=path.split('/')
+  @y=[]
+  (0...x.length).each do |i|
+    (0...i+1).each do |j|
+      if !@y[i].nil?
+        @y[i]="#{@y[i]}/#{x[j]}"
+        else
+        @y[i]=x[j]
       end
-      puts "child= #{children}"
-      children.each do |ch|
-          puts"hash = #{@shash}"
-          newarray=get_subarray(ch,subarray)
-          puts "sub array = #{newarray}"
-          @shash[ch]=newarray
-          puts "hash= #{@shash}"
-        end
-  return subdir
+    end
+  end
+  return @y
 end
 
-def get_subarray(child,subarray)
-  newarray=[]
-  puts"function#{subarray}"
-  subarray.each do |s|
-    c=s.split('/')
-    if c.second==child
-      newarray<<c .last
+def callHash(hashtree)
+  list=""
+  hashtree.each do |key,value|
+    if key.include?('/')
+      list<<"<li>#{key.split('/').last}"
+    else
+      list<<"<li>#{key}"
+    end
+    if value.is_a?(Array)
+      list<<callArray(value)
+    else
+      list<<"</li>"
+    end
   end
+  list<<"</li>"
+  return list
 end
-return newarray
+
+def callArray(value)
+  sublist="<ul>"
+  value.each do |v|
+    unless v==nil
+      if v.is_a?(Hash)
+        sublist<<callHash(v)
+      else
+        if v.include?('/')
+          sublist<<"<li>#{v.split('/').last}</li>"
+        else
+          sublist<<"<li>#{v}</li>"
+        end
+      end
+    end
+  end
+  sublist<<"</ul></li>"
+  return sublist
 end
 end
