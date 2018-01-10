57dc04fd795c6c6f5710c7e468c01f851727fcd9
amitoj singh <amitojsingh95@gmail.com>
2018-01-10 12:10:31 +0530

    postgree

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,15 @@
 
 ActiveRecord::Schema.define(version: 20170525110358) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "gitrepos", force: :cascade do |t|
     t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
-    t.index ["user_id"], name: "index_gitrepos_on_user_id"
+    t.index ["user_id"], name: "index_gitrepos_on_user_id", using: :btree
   end
 
   create_table "repositories", force: :cascade do |t|
@@ -28,7 +31,7 @@ ActiveRecord::Schema.define(version: 20170525110358) do
     t.integer  "upload_file_size"
     t.datetime "upload_updated_at"
     t.integer  "user_id"
-    t.index ["user_id"], name: "index_repositories_on_user_id"
+    t.index ["user_id"], name: "index_repositories_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -46,8 +49,8 @@ ActiveRecord::Schema.define(version: 20170525110358) do
     t.datetime "updated_at",                          null: false
     t.string   "provider"
     t.string   "uid"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
